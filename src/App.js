026551856297c875646cc2f2372efd5850bc6f67
Charles Tyner <c.tyner@hotmail.com>
2024-09-04 07:10:37 -0400

    workshop 2

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import './App.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import CampsiteDetailPage from './pages/CampsiteDetailPage';
+import AboutPage from './pages/AboutPage';
 
 function App() {
   return (
@@ -14,7 +15,8 @@ function App() {
       <Routes>
         <Route path='/' element={<HomePage />} />
         <Route path='contact' element={<ContactPage />} />
-        <Route path='directory' element={<CampsitesDirectoryPage />}/>
+        <Route path='directory' element={<CampsitesDirectoryPage />} />
+        <Route path='about' element={<AboutPage />} />
         <Route
             path='directory/:campsiteId'
             element={<CampsiteDetailPage />}
